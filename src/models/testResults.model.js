0efb2af819221823a8171f4beaf89c072c576498
MauricioSantos12 <mauricio.santosr12@gmail.com>
2025-11-14 12:45:32 -0500

    fix: totalPages in getAllResults

--- src/models/testResults.model.js
+++ src/models/testResults.model.js
@@ -32,42 +32,21 @@ class TestResultModel extends BaseModel {
   }
 
   async getAllResults({ page = 1, pageSize = 10, filters = {} }) {
-    const totalCount = await knex("test_results").count("*").first();
     const query = knex("test_results").select("*");
     if (Object.keys(filters).length > 0) {
       Object.keys(filters).forEach((key) => {
         query.where(key, filters[key]);
       });
     }
+    const totalData = await query;
+    const totalRows = totalData.length;
     const data = await query.offset((page - 1) * pageSize).limit(pageSize);
+
     return {
       data,
-      totalPages: Math.ceil(totalCount["count(*)"] / pageSize),
+      totalPages: Math.ceil(totalRows / pageSize),
     };
   }
-
-  // const query = knex("test_results as tr")
-  //   .select(
-  //     "tr.*",
-  //     knex.raw(`
-  //       JSON_OBJECT(
-  //         'id', u.id,
-  //         'name', u.name,
-  //         'email', u.email
-  //       ) as userInfo
-  //     `),
-
-  //     knex.raw(`
-  //       JSON_OBJECT(
-  //         'id', t.id,
-  //         'name', t.name,
-  //         'description', t.description
-  //         /* Add any other columns from the 'tests' table here */
-  //       ) as testInfo
-  //     `)
-  //   )
-  //   .join("users as u", "tr.user_id", "u.id")
-  //   .join("tests as t", "tr.test_id", "t.id");
 }
 
 export default new TestResultModel();
