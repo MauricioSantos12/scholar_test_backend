797dc23e174b281f5c72e0702b0956ce52e49d56
MauricioSantos12 <mauricio.santosr12@gmail.com>
2025-11-15 14:53:20 -0500

    fix: dont delete all the users in each seed

--- src/db/seeds/seed_users.js
+++ src/db/seeds/seed_users.js
@@ -2,68 +2,89 @@ import bcrypt from "bcrypt";
 
 export async function seed(knex) {
   // Eliminar usuarios previos
-  await knex("users").del();
 
   const hashedAdmin = await bcrypt.hash("admin123", 10);
   const hashedStudent = await bcrypt.hash("student123", 10);
 
-  await knex("users").insert([
-    {
-      identification_number: "100000001",
-      name: "Admin",
-      second_name: null,
-      last_name: "",
-      email: "admin@example.com",
-      password: hashedAdmin,
-      date_birth: "1990-01-01",
-      school_name: "EduTest",
-      graduation_year: 2010,
-      city: "Ibagué",
-      phone_number: "3000000000",
-      parent_name: null,
-      parent_email: null,
-      parent_phone: null,
-      role: "admin",
-      created_at: new Date(),
-      updated_at: new Date(),
-    },
-    {
-      identification_number: "200000001",
-      name: "Camila",
-      second_name: "",
-      last_name: "Leyva",
-      email: "camila.leyva@example.com",
-      password: hashedStudent,
-      date_birth: "2005-06-15",
-      school_name: "Liceo Nacional",
-      graduation_year: 2017,
-      city: "Ibagué",
-      phone_number: "311112222333",
-      parent_name: "Diana Escalante",
-      parent_email: "diana@example.com",
-      parent_phone: "311112222333",
-      role: "student",
-      created_at: new Date(),
-      updated_at: new Date(),
-    },
-    {
-      identification_number: "200000002",
-      name: "Mauricio",
-      second_name: "",
-      last_name: "Santos",
-      email: "mauricio.santos@example.com",
-      password: hashedStudent,
-      date_birth: "2005-06-15",
-      school_name: "Comfenalco",
-      graduation_year: 2015,
-      city: "Ibagué",
-      phone_number: "311112222333",
-      parent_name: "Maria Rebolledo",
-      parent_email: "maria@example.com",
-      parent_phone: "311112222333",
-      role: "student",
-      created_at: new Date(),
-      updated_at: new Date(),
-    },
-  ]);
+  const adminExist = await knex("users")
+    .where({ email: "admin@example.com" })
+    .first();
+  const student1Exist = await knex("users")
+    .where({ email: "camila.leyva@example.com" })
+    .first();
+  const student2Exist = await knex("users")
+    .where({ email: "mauricio.santos@example.com" })
+    .first();
+
+  if (!adminExist) {
+    await knex("users").insert([
+      {
+        identification_number: "100000001",
+        name: "Admin",
+        second_name: null,
+        last_name: "",
+        email: "admin@example.com",
+        password: hashedAdmin,
+        date_birth: "1990-01-01",
+        school_name: "EduTest",
+        graduation_year: 2010,
+        city: "Ibagué",
+        phone_number: "3000000000",
+        parent_name: null,
+        parent_email: null,
+        parent_phone: null,
+        role: "admin",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ]);
+  }
+
+  if (!student1Exist) {
+    await knex("users").insert([
+      {
+        identification_number: "200000001",
+        name: "Camila",
+        second_name: "",
+        last_name: "Leyva",
+        email: "camila.leyva@example.com",
+        password: hashedStudent,
+        date_birth: "2005-06-15",
+        school_name: "Liceo Nacional",
+        graduation_year: 2017,
+        city: "Ibagué",
+        phone_number: "311112222333",
+        parent_name: "Diana Escalante",
+        parent_email: "diana@example.com",
+        parent_phone: "311112222333",
+        role: "student",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ]);
+  }
+
+  if (!student2Exist) {
+    await knex("users").insert([
+      {
+        identification_number: "200000002",
+        name: "Mauricio",
+        second_name: "",
+        last_name: "Santos",
+        email: "mauricio.santos@example.com",
+        password: hashedStudent,
+        date_birth: "2005-06-15",
+        school_name: "Comfenalco",
+        graduation_year: 2015,
+        city: "Ibagué",
+        phone_number: "311112222333",
+        parent_name: "Maria Rebolledo",
+        parent_email: "maria@example.com",
+        parent_phone: "311112222333",
+        role: "student",
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ]);
+  }
 }
